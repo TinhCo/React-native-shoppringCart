c44e316496d4747ceffc500d245ed4ff7d9fb3aa
TinhCo <phungtoan872@gmail.com>
2023-10-01 09:26:18 +0700

    toan

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+require('dotenv').config()
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
+const cookieParser = require('cookie-parser')
+
+
+const app = express()
+app.use(cookieParser())
+const port = process.env.PORT || 5000
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+dbConnect()
+initRoutes(app)
+
+app.listen(port, () => {
+    console.log('Server running on the port: ' + port);
+})
+//facebook
